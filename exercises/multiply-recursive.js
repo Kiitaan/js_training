0b28afb5cd1dad2a99c0ef0c5fee9ff1e40da035
Christian LIM <christian.lim@outlook.fr>
2018-04-26 12:58:55 +0200

    exo

--- exercises/multiply-recursive.js
+++ exercises/multiply-recursive.js
@@ -1,4 +1,4 @@
-'use strict'
+//'use strict'
 
 /*
  * Create a function `multiply` that takes two number arguments
@@ -10,6 +10,7 @@
  */
 
 // Your code :
+
 
 let i = 0
 let result = 0
@@ -31,23 +32,36 @@ const multiply = (nb1, nb2) => {
 
 // console.log("c le premier passage de i =" , i)
 // console.log("premier de result",result)
-const res = multiply(10, 1)
+//const res = multiply(10, 1)
+//console.log('result:', res)
+//console.log("2eme i :", i)
+//console.log("second result",result)
+
+//console.log('console log de multiply au second tour',multiply(10,5))
+
+
+
+
+
+
+
+
 
 //* Begin of tests
-const assert = require('assert')
+// const assert = require('assert')
 
-assert.strictEqual(typeof multiply, 'function')
-assert.strictEqual(multiply.length, 2)
-assert.strictEqual(multiply.toString().includes('Math.imul'), false)
-assert.strictEqual(multiply.toString().includes('while'), false)
-assert.strictEqual(multiply.toString().includes('for'), false)
-assert.strictEqual(multiply.toString().includes('*'), false)
-assert.strictEqual(multiply.toString().includes('/'), false)
-assert.strictEqual(multiply(34, 78), 2652)
-assert.strictEqual(multiply(123, 0), 0)
-assert.strictEqual(multiply(0, -230), 0)
-assert.strictEqual(multiply(0, 0), 0)
-assert.strictEqual(multiply(123, -22), -2706)
-assert.strictEqual(multiply(-22, 123), -2706)
-assert.strictEqual(multiply(-22, -123), 2706)
+// assert.strictEqual(typeof multiply, 'function')
+// assert.strictEqual(multiply.length, 2)
+// assert.strictEqual(multiply.toString().includes('Math.imul'), false)
+// assert.strictEqual(multiply.toString().includes('while'), false)
+// assert.strictEqual(multiply.toString().includes('for'), false)
+// assert.strictEqual(multiply.toString().includes('*'), false)
+// assert.strictEqual(multiply.toString().includes('/'), false)
+// assert.strictEqual(multiply(34, 78), 2652)
+// assert.strictEqual(multiply(123, 0), 0)
+// assert.strictEqual(multiply(0, -230), 0)
+// assert.strictEqual(multiply(0, 0), 0)
+// assert.strictEqual(multiply(123, -22), -2706)
+// assert.strictEqual(multiply(-22, 123), -2706)
+// assert.strictEqual(multiply(-22, -123), 2706)
 // End of tests */
